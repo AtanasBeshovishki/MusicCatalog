18f2bed2b1f4903d1b0eca55df426369d141ed12
AtanasBeshovishki <atanas@next-stream.com>
2016-08-01 14:34:24 +0300

    register
    add register form, show register page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,28 @@
+class UserController {
+    register(data) {
+        if (data.username.length < 6) {
+            showPopup('error', 'The username...');
+            return;
+        }
+        if (data.fullname.length < 5) {
+            showPopup('error', 'The fullname...');
+            return;
+        }
+        if (data.password != data.confirmPassword) {
+            showPopup('error', 'The password...');
+            return;
+        }
+        if (data.password.length < 8) {
+            showPopup('error', 'The password...');
+            return;
+        }
 
+        delete data['confirmPassword'];
+        this.register.post(this._baseServiceUrl, data, function successCallback(response) {
+            showPopup('success', 'Register successful');
+            redirectUrl('#/login');
+        }, function errorCallback(response) {
+            showPopup('error', "Error, ....");
+        });
+    }
+}
--- views/homeView.js
+++ views/homeView.js
@@ -4,7 +4,7 @@ class HomeView {
         this._wrapperSelector = wrapperSelector;
     }
 
-    showGuestPage(sidebarData, mainData) {
+    showGuestPage() {
         let _that = this;
         $.get('templates/welcome-guest.html', function (template) {
             let renderedWrapper = Mustache.render(template, null);
@@ -27,6 +27,48 @@ class HomeView {
             // });
         });
     }
+
+
+    showRegisterPage() {
+        let _that = this;
+        $.get('templates/register.html', function (template) {
+            let renderedWrapper = Mustache.render(template, null);
+
+            $(_that._wrapperSelector).html(renderedWrapper);
+            $('#register-request-button').on('click', function () {
+                console.log('eeee');
+                let username = $('#username').val();
+                let password = $('#password').val();
+                let confirmPassword = $('#pass-confirm').val();
+                let fullName = $('#full-name').val();
+
+                let data = {
+                    username:username,
+                    password:password,
+                    fullname:fullName,
+                    confirmPassword:confirmPassword
+                };
+                console.log(data);
+                triggerEvent('register', data);
+            });
+            // $.get('templates/posts.html', function (template) {
+            //     let blogPosts = {
+            //         blogPosts:mainData
+            //     };
+            //     let renderedPosts = Mustache.render(template, blogPosts);
+            //     $('.articles').html(renderedPosts);
+            // });
+
+            // $.get('templates/recent-posts.html', function (template) {
+            //     let recentPosts = {
+            //         recentPosts:sidebarData
+            //     };
+            //     let renderedPosts = Mustache.render(template, recentPosts);
+            //     $('.recent-posts').html(renderedPosts);
+            // });
+        });
+    }
+
 
     showUserPage(sidebarData, mainData) {
         let _that = this;
